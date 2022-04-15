2615aaf63384bbef3a1c9ac870f1030187e9541c
lmarello <leomarello@gmail.com>
2022-04-15 20:41:09 -0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  basePath: "/weather-app",
-  assetPrefix: "/weather-app",
 };
 
 module.exports = nextConfig;
